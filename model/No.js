5dc7d0e31da6bc0b64b161908e4f68c453b163a2
Sergio Souza Novak <serginhosnovak@hotmail.com>
2018-04-21 17:00:53 -0300

    criado primeiro esboço da classe No

---
+++ model/No.js
@@ -0,0 +1,21 @@
+
+
+//classe No
+
+function No(){
+
+    this.nomeDeCidade;
+    var listaAdjacencia;
+
+    function No(nomeDeCidade){
+        this.nomeDeCidade = nomeDeCidade;
+    }
+
+    function No(nomeDeCidade,listaAdjacencia){
+        this.listaAdjacencia = listaAdjacencia;
+        this.nomeDeCidade = nomeDeCidade;
+    }
+}
+
+
+
